bb3a36883cbdd6543030b964f011006a3eee99b9
gkwlsdn17 <gkwlsdn17@gmail.com>
2022-11-16 22:58:14 +0900

    고객수정 추가

--- crm/main/urls.py
+++ crm/main/urls.py
@@ -4,8 +4,10 @@ from .views import main_views, customer_views
 urlpatterns = [
     path('', main_views.index),
     path('customer/', customer_views.selectCustomer, name="customer_home"),
-    path('customer/insert/' , customer_views.insertCustomer),
-    path('customer/insert/view/', customer_views.pageInsertCustomer),
+    path('customer/insert/' , customer_views.insertCustomer, name="customer_insert"),
+    path('customer/insert/view/', customer_views.pageInsertCustomer, name="customer_insert_view"),
+    path('customer/detail/<str:customer_id>', customer_views.pageCustomerDetail, name="customer_detail"),
+    path('customer/update/', customer_views.updateCustomer, name="customer_update"),
     
     
 
--- crm/main/views/customer_views.py
+++ crm/main/views/customer_views.py
@@ -45,3 +45,37 @@ def insertCustomer(request):
     customer.save()
     
     return redirect("customer_home")
+
+@login_required
+def pageCustomerDetail(request, customer_id):
+
+    if customer_id is None:
+        return redirect("customer_home")
+
+    customer = Customer.objects.get(CUSTOMER_ID=customer_id)
+    content = {'customer': customer}
+    return render(request, 'main/customer_detail.html', content)
+
+@login_required
+def updateCustomer(request):
+    try:
+        id = request.POST.get('customer_id')
+        name = request.POST.get('customer_name')    
+        birth = request.POST.get('customer_birth')
+        phone = request.POST.get('customer_phone')
+        email = request.POST.get('customer_email')
+        addr = request.POST.get('customer_addr')
+        rating = request.POST.get('customer_rating')
+        customer = Customer.objects.get(CUSTOMER_ID = id)
+        customer.CUSTOMER_NAME = name
+        customer.CUSTOMER_BIRTH = birth
+        customer.CUSTOMER_PHONE = phone
+        customer.CUSTOMER_EMAIL = email
+        customer.CUSTOMER_ADDR = addr
+        customer.CUSTOMER_RATING = rating
+        customer.save()
+        return redirect("customer_home")
+    except:
+        redirect('customer_detail', customer_id=id)
+
+    
