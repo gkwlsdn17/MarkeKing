78f71c3b820bf90d0422f8c78f18ab2a0cc7cc84
gkwlsdn17 <gkwlsdn17@gmail.com>
2023-01-08 20:27:07 +0900

    DB 추가

--- crm/main/models.py
+++ crm/main/models.py
@@ -34,3 +34,35 @@ class Goods(models.Model):
 class GoodsType(models.Model):
     NAME = models.CharField(max_length=100)
     DISCARD = models.BooleanField(default=False)
+
+class Order(models.Model):
+    CUSTOMER_NO = models.ForeignKey('Customer', on_delete=models.DO_NOTHING)
+    GOODS_NO = models.ForeignKey('Goods', on_delete=models.DO_NOTHING)
+    GOODS_NAME = models.CharField(max_length=200)
+    GOODS_COUNT = models.IntegerField(default=0)
+    PRICE = models.IntegerField(default=0)
+    TOTAL_AMOUNT = models.IntegerField(default=0)
+    CRTIME = models.DateTimeField(auto_now=True)
+    DISCARD = models.BooleanField(default=False)
+
+class Delivery(models.Model):
+    ORDER_NO = models.ForeignKey('Delivery', on_delete=models.DO_NOTHING, default=0)
+    CUSTOMER_NO = models.ForeignKey('Customer', on_delete=models.DO_NOTHING)
+    DELIVERY_NAME = models.CharField(max_length=200)
+    DELIVERY_ADDR = models.CharField(max_length=1024)
+    DELIVERY_PHONE = models.CharField(max_length=100)
+    DELIVERY_STATUS = models.ForeignKey('DeliveryStatus', on_delete=models.DO_NOTHING, default=0)
+    DELIVERY_COMPANY = models.ForeignKey('DeliveryCompany', on_delete=models.DO_NOTHING, default=0)
+    CRTIME = models.DateTimeField(auto_now=True)
+    DISCARD = models.BooleanField(default=False)
+
+class DeliveryStatus(models.Model):
+    STATUS = models.CharField(max_length=512)
+    CRTIME = models.DateTimeField(auto_now=True)
+    DISCARD = models.BooleanField(default=False)
+
+class DeliveryCompany(models.Model):
+    COMPANY_NAME = models.CharField(max_length=512)
+    COMPANY_PHONE = models.CharField(max_length=100)
+    CRTIME = models.DateTimeField(auto_now=True)
+    DISCARD = models.BooleanField(default=False)
