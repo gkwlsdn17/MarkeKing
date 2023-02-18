c5e7df12f4888e9ec3e02f2b682ed70dad7614b4
gkwlsdn17 <gkwlsdn17@gmail.com>
2023-02-18 20:07:33 +0900

    crm 메인화면 수정

--- crm/main/views/main_views.py
+++ crm/main/views/main_views.py
@@ -4,6 +4,7 @@ import logging
 import datetime
 from ..models import *
 from django.db.models import *
+from django.db.models.functions import RowNumber
 from dateutil.relativedelta import relativedelta
 logger = logging.getLogger('main')
 
@@ -25,11 +26,20 @@ def index(request):
         weekOrderCount = orderCount((today + relativedelta(days=-weekday)).strftime('%Y%m%d'+"000000"), (today + relativedelta(days=7-weekday)).strftime('%Y%m%d%H%M%S'))
         monthOrderCount = orderCount(today.strftime('%Y%m')+"01000000", (today + relativedelta(days=1)).strftime('%Y%m%d%H%M%S'))
 
+
+        # 주문진행중
+        orderList = orderStatus()
+        
+        # 회원가입 숫자
+        customerCnt = customerStatus()
+        print(customerCnt)
+
         content = {
             'weekStartDate': (today + relativedelta(days=-weekday)).strftime('%Y-%m-%d'),
             'weekEndDate': (today + relativedelta(days=7-1-weekday)).strftime('%Y-%m-%d'),
             'todayCount': todayCount, 'weekCount': weekCount, 'monthCount': monthCount,
             'todayOrderCount': todayOrderCount, 'weekOrderCount': weekOrderCount, 'monthOrderCount': monthOrderCount,
+            'orderList': orderList, 'customerCnt': customerCnt,
             }
         return render(request, 'main/index.html', content)
     else:
@@ -60,3 +70,43 @@ def orderCount(startDate, endDate):
     except Exception as e:
         logger.error(e)
         return 0
+
+# 주문진행중
+def orderStatus():
+    try:
+        q = Q()
+        q &= Q(DISCARD = False)
+        q &= Q(DELIVERY_STATUS_id__lt = 4)
+
+        list = Delivery.objects.filter(q).values('CUSTOMER_NO__CUSTOMER_ID', 'CUSTOMER_NO__CUSTOMER_NAME', 'DELIVERY_STATUS__STATUS', 'id').annotate(
+            row = Window(expression=RowNumber(), order_by=F('id').desc())
+        ).order_by('row')[:5]
+        return list
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def customerStatus():
+    try:
+        q = Q()
+        q &= Q(DISCARD = False)
+
+        allCnt = Customer.objects.filter(q).count()
+
+        today = datetime.datetime.today()
+        start = today + relativedelta(days=-6)
+
+        q &= Q(FIRST_VISIT__range=(start.strftime('%Y-%m-%d'), (today + relativedelta(days=1)).strftime('%Y-%m-%d')))
+        weekCnt = Customer.objects.filter(q).count()
+        todayCnt = Customer.objects.all().filter(FIRST_VISIT__range=(today, (today + relativedelta(days=1)).strftime('%Y-%m-%d'))).count()
+
+        content = {
+            'allCnt': allCnt,
+            'weekCnt': weekCnt,
+            'todayCnt': todayCnt
+        }
+
+        return content
+    except Exception as e:
+        logger.error(e)
+        return None
